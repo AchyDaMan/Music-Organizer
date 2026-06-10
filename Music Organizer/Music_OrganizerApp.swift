0e49ca3c5f3fcd22b0129de02fb951cf969bc77c
AchyDaMan <90523261+AchyDaMan@users.noreply.github.com>
2026-06-10 15:19:51 -0400

    Initial Commit

---
+++ Music Organizer/Music_OrganizerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Music_OrganizerApp.swift
+//  Music Organizer
+//
+//  Created by Achintya Yedavalli on 6/10/26.
+//
+
+import SwiftUI
+
+@main
+struct Music_OrganizerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Music OrganizerTests/Music_OrganizerTests.swift
@@ -0,0 +1,19 @@
+//
+//  Music_OrganizerTests.swift
+//  Music OrganizerTests
+//
+//  Created by Achintya Yedavalli on 6/10/26.
+//
+
+import Testing
+@testable import Music_Organizer
+
+struct Music_OrganizerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
